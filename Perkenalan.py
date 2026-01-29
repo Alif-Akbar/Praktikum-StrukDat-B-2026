fe7098d7133fc8362e404df060208eaff353b063
Alif-Akbar <alifakbarpku31808@gmail.com>
2026-01-29 09:40:33 +0700

    biodata.md

---
+++ Perkenalan.py
@@ -0,0 +1,12 @@
+
+print("Hello, World")
+
+#profil mahasiswa
+
+name = "Muhammad_Averroes_Alif_Akbar"
+
+NIM = "25071103405"
+
+Prodi = "S1_Teknik_Informatika"
+
+print("Halo teman-teman, nama saya" , name , "dari prodi" , Prodi , "dengan NIM" , NIM )
---
+++ biodata.md
@@ -0,0 +1,5 @@
+Nama : Muhammad Averroes Alif Akbar
+
+NIM : 25071103405
+
+Prodi : S1 Teknik Informatika
